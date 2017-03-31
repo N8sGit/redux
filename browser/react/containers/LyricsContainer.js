f2e7597f22b16436ea22b7ab2d1dfddf8cda2540
Judah Hamaoui <ayhamaoui@gmail.com>
2017-03-31 11:40:42 -0400

    completed Lyrics Container

---
+++ browser/react/containers/LyricsContainer.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import store from '../store'
+import Lyrics from '../components/Lyrics'
+import {setLyrics} from '../action-creators/lyrics'
+import axios from 'axios'
+
+export default class LyricssContainer extends React.Component {
+  constructor(){
+    super()
+    // this.state = { ...store.getState(), artistQuery: '', songQuery: ''}
+    this.state = Object.assign({
+      artistQuery: '',
+      songQuery: ''
+    },store.getState());
+
+    this.setArtist = this.setArtist.bind(this)
+    this.setSong = this.setSong.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+
+  componentDidMount(){
+    this.unsubscribe = store.subscribe(() => {
+      this.setState(store.getState());
+    });
+  }
+  componentWillUnmount(){
+    this.unsubscribe()
+  }
+
+  setArtist(artist){
+    this.setState({artistQuery: artist})
+  }
+
+  setSong(song){
+    this.setState({songQuery: song})
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    if (this.state.artistQuery && this.state.songQuery)
+    console.log(`/api/lyrics/${this.state.artistQuery}/${this.state.songQuery}`)
+    axios.get(`/api/lyrics/${this.state.artistQuery}/${this.state.songQuery}`)
+    .then(res => res.data)
+    .then(data => {
+      console.log(data)
+      const setLyricsAction = setLyrics(data.lyric)
+      store.dispatch(setLyricsAction);
+    })
+  }
+
+  render(){
+    return (
+      <Lyrics
+        text={this.state.text}
+        artistQuery={this.state.artistQuery}
+        songQuery={this.state.songQuery}
+        setArtist={this.setArtist}
+        setSong={this.setSong}
+        handleSubmit={this.handleSubmit}
+      />
+    )
+  }
+}
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,33 +1,3 @@
-import store from './store';
-import {setLyrics} from './action-creators/lyrics';
-
-console.log('-------------------------');
-console.log('State before any actions: ', store.getState());
-
-const inTheAirTonightAction = setLyrics('I can feel it coming in the air tonight ... hold on ...');
-store.dispatch(inTheAirTonightAction);
-
-console.log('-------------------------');
-console.log('State after first SET_LYRICS action: ', store.getState());
-
-const rickRollAction = setLyrics('Never gonna give you up, never gonna let you down');
-store.dispatch(rickRollAction);
-
-console.log('-------------------------');
-console.log('State after second SET_LYRICS action: ', store.getState());
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
@@ -39,6 +9,7 @@ import Artist from './components/Artist';
 import Songs from './components/Songs';
 import NewPlaylistContainer from './containers/NewPlaylistContainer';
 import Playlist from './components/Playlist';
+import LyricsContainer from './containers/LyricsContainer'
 
 ReactDOM.render(
   <Router history={hashHistory}>
@@ -52,6 +23,7 @@ ReactDOM.render(
       </Route>
       <Route path="/new-playlist" component={NewPlaylistContainer} />
       <Route path="playlists/:playlistId" component={Playlist} />
+      <Route path="/lyrics" component={LyricsContainer}/>
       <IndexRedirect to='/albums' />
     </Route>
   </Router>,
